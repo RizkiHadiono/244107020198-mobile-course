8ee928cbb3dd69e1ba5248a820d92edff8d415e0
Rizkihadiono <rizkihadiono95@gmail.com>
2026-08-30 20:44:01 +0700

    Tugas Praktikum Profil Mahasiswa

--- _01_week_1_mobile_development_ecosystem_flutter_refresh/lib/main.dart
+++ _01_week_1_mobile_development_ecosystem_flutter_refresh/lib/main.dart
@@ -12,10 +12,22 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(title: const Text('Profil Mahasiswa')),
         body: const Center(
           child: Column(mainAxisSize: MainAxisSize.min, children: [
-            Icon(Icons.school, size: 72),
-            SizedBox(height: 16),
+            Icon(Icons.school, size: 100, color: Colors.blue),
+            Text('Politeknik Negeri Malang', style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold)),
+            SizedBox(height: 20),
+            Icon(Icons.person, size: 80, color: Colors.green),
             Text('Mokhamad Rizki Hadiono Singgih', style: TextStyle(fontSize: 24)),
-            Text('Pemrograman Mobile — Minggu 1'),
+            SizedBox(height: 8),
+            Text('NIM : 2441070198', style: TextStyle(fontSize: 20)),
+            SizedBox(height: 15),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center, 
+              children: [
+                Icon(Icons.book, color: Colors.orange),
+                SizedBox(width: 8), 
+                Text('Pemrograman Mobile — Minggu 1'),
+              ],
+            )
           ]),
         ),
       ),
